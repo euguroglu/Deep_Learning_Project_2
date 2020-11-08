af1887000500056040ab4c4b51ea7eab223c5821
euguroglu <euguroglu@yahoo.com>
2020-11-09 00:43:29 +0300

    Training Scrpit Added

--- Packages/trainer.py
+++ Packages/trainer.py
@@ -1,8 +1,9 @@
-from tensorflow.keras.preprocessing import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.layers import Dropout,Flatten,Dense,Input
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizer import SGD,Adam
+from tensorflow.keras.optimizers import SGD,Adam
+import os
 
 def build_model(nbr_classes):
 
@@ -30,7 +31,7 @@ def build_data_pipelines(batch_size,train_data_path,val_data_path,eval_data_path
         rotation_range = 25,
         zoom_range = 0.15,
         width_shift_range = 0.2,
-        height_shift_Range = 0.2,
+        height_shift_range = 0.2,
         shear_range = 0.15,
         horizontal_flip = True,
         fill_mode ='nearest'
@@ -44,7 +45,7 @@ def build_data_pipelines(batch_size,train_data_path,val_data_path,eval_data_path
         train_data_path,
         class_mode='categorical',
         target_size = (229,229),
-        color_mode = "rgb"
+        color_mode = "rgb",
         shuffle = True,
         batch_size = batch_size
     )
@@ -53,7 +54,7 @@ def build_data_pipelines(batch_size,train_data_path,val_data_path,eval_data_path
         val_data_path,
         class_mode='categorical',
         target_size = (229,229),
-        color_mode = "rgb"
+        color_mode = "rgb",
         shuffle = False,
         batch_size = batch_size
     )
@@ -62,8 +63,43 @@ def build_data_pipelines(batch_size,train_data_path,val_data_path,eval_data_path
         eval_data_path,
         class_mode='categorical',
         target_size = (229,229),
-        color_mode = "rgb"
+        color_mode = "rgb",
         shuffle = False,
         batch_size = batch_size
     )
     return train_generator,val_generator,eval_generator
+
+
+
+def train(path_to_data,batch_size,epochs):
+
+    path_train_data = os.path.join(path_to_data,'training')
+    path_val_data = os.path.join(path_to_data,'validation')
+    path_eval_data = os.path.join(path_to_data,'evaluation')
+
+    train_generator,val_generator,eval_generator = build_data_pipelines(
+        batch_size=batch_size,
+        train_data_path=path_train_data,
+        val_data_path=path_val_data,
+        eval_data_path=path_eval_data
+    )
+
+    model = build_model(nbr_classes=11)
+
+    optimizer = Adam(lr=1e-5)
+
+    model.compile(loss='categorical_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch= 9000 // batch_size,
+        validation_data=val_generator,
+        validation_steps= 3500 // batch_size,
+        epochs = epochs
+    )
+
+
+if __name__ =="__main__":
+
+    path_to_data = 'C:/Users/eugur/Deep_Learning_Deployment/food-11/'
+    train(path_to_data,2,2)
