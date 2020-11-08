6a6e979ff137d37e9951300eeac184922419f9e8
euguroglu <euguroglu@yahoo.com>
2020-11-08 23:53:06 +0300

    Data augmentation added

--- Packages/trainer.py
+++ Packages/trainer.py
@@ -21,3 +21,49 @@ def build_model(nbr_classes):
         layer.trainable = False
 
     return model
+
+
+def build_data_pipelines(batch_size,train_data_path,val_data_path,eval_data_path):
+
+    train_augmentor = ImageDataGenerator(
+        rescale = 1. / 255,
+        rotation_range = 25,
+        zoom_range = 0.15,
+        width_shift_range = 0.2,
+        height_shift_Range = 0.2,
+        shear_range = 0.15,
+        horizontal_flip = True,
+        fill_mode ='nearest'
+    )
+
+    val_augmentor = ImageDataGenerator(
+        rescale = 1./255
+    )
+
+    train_generator = train_augmentor.flow_from_directory(
+        train_data_path,
+        class_mode='categorical',
+        target_size = (229,229),
+        color_mode = "rgb"
+        shuffle = True,
+        batch_size = batch_size
+    )
+
+    val_generator = val_augmentor.flow_from_directory(
+        val_data_path,
+        class_mode='categorical',
+        target_size = (229,229),
+        color_mode = "rgb"
+        shuffle = False,
+        batch_size = batch_size
+    )
+
+    eval_generator = val_augmentor.flow_from_directory(
+        eval_data_path,
+        class_mode='categorical',
+        target_size = (229,229),
+        color_mode = "rgb"
+        shuffle = False,
+        batch_size = batch_size
+    )
+    return train_generator,val_generator,eval_generator
