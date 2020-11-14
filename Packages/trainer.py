ca0bf64fb3bb77dde80f1dabf249a3f312733305
euguroglu <euguroglu@yahoo.com>
2020-11-14 18:34:40 +0300

    Open cv problem solved

--- Packages/trainer.py
+++ Packages/trainer.py
@@ -6,10 +6,13 @@ from tensorflow.keras.optimizers import SGD,Adam
 import os
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
-from DataHandler import download_data_to_local_directory
+from DataHandler import download_data_to_local_directory, upload_data_to_bucket
 from tensorflow.python.client import device_lib
 import argparse
 import hypertune
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import shutil
+from datetime import datetime
 
 print("Tensorflow is running on following devices: ")
 print(device_lib.list_local_devices())
@@ -22,7 +25,7 @@ def build_model(nbr_classes):
     head_model = base_model.output
 
     head_model = Flatten()(head_model)
-    head_model = Dense(512)(head_model)
+    head_model = Dense(512,activation="relu")(head_model)
     head_model = Dropout(0.5)(head_model)
     head_model = Dense(nbr_classes, activation="softmax")(head_model)
 
@@ -91,7 +94,7 @@ def get_number_of_imgs_inside_folder(directory):
     return totalcount
 
 
-def train(path_to_data,batch_size,epochs,learning_rate):
+def train(path_to_data,batch_size,epochs,learning_rate,models_bucket_name):
 
     path_train_data = os.path.join(path_to_data,'training')
     path_val_data = os.path.join(path_to_data,'validation')
@@ -118,12 +121,26 @@ def train(path_to_data,batch_size,epochs,learning_rate):
 
     model.compile(loss='categorical_crossentropy',optimizer=optimizer,metrics=['accuracy'])
 
+    early_stopping = EarlyStopping(monitor="val_loss",patience=10)
+
+    path_to_save_model = "./tmp"
+    if not os.path.isdir(path_to_save_model):
+        os.makedirs(path_to_save_model)
+
+    checkpoint_saver = ModelCheckpoint(path_to_save_model,
+                                        monitor="val_accuracy",
+                                        mode="max",
+                                        save_best_only=True,
+                                        save_freq="epoch",
+                                        verbose=1)
+
     model.fit_generator(
         train_generator,
         steps_per_epoch= total_train_imgs // batch_size,
         validation_data=val_generator,
         validation_steps= total_val_imgs // batch_size,
-        epochs = epochs
+        epochs = epochs,
+        callbacks=[early_stopping,checkpoint_saver]
     )
 
     print('[INFO] Evaluation phase...')
@@ -143,6 +160,14 @@ def train(path_to_data,batch_size,epochs,learning_rate):
     print("Done evaluating!")
     loss = scores[0]
     print(f"loss for hyptertune = {loss}")
+
+    now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    zipped_folder_name = f"trained_model_{now}_loss_{loss}"
+    shutil.make_archive(zipped_folder_name,"zip","/usr/src/app/tmp")
+
+    path_zipped_folder =  "/usr/src/app/"+zipped_folder_name+".zip"
+    upload_data_to_bucket(models_bucket_name,path_zipped_folder,zipped_folder_name)
+
     hpt = hypertune.HyperTune()
     hpt.report_hyperparameter_tuning_metric(hyperparameter_metric_tag='loss',
                                             metric_value=loss, global_step=epochs)
@@ -154,7 +179,9 @@ if __name__ =="__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--bucket_name",type=str, help="Bucket name on Google cloud storage",
-                        default='dummy-bucket-food-dataset')
+                        default='enes-data-bucket')
+    parser.add_argument("--models_bucket_name",type=str, help="Bucket name on Google cloud storage for saving trained models",
+                        default='trained_models_food_classification_bucket')
     parser.add_argument("--batch_size",type=int, help="Batch size used by the deep learning model",
                         default=2)
     parser.add_argument("--learning_rate",type=float, help="Learning rate used by the deep learning model",
@@ -167,4 +194,4 @@ if __name__ =="__main__":
     download_data_to_local_directory(args.bucket_name,'C:/Users/eugur/Deep_Learning_Deployment/data')
     print('Download finished...')
     path_to_data = 'C:/Users/eugur/Deep_Learning_Deployment/data'
-    train(path_to_data,args.batch_size,10,args.learning_rate)
+    train(path_to_data,args.batch_size,20,args.learning_rate,args.models_bucket_name)
